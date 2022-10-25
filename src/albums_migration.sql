8049b9dc2f286a4bdca2b5634f9092184a378b98
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-25 13:47:58 -0500

    removed NOT NULL from columns

--- src/albums_migration.sql
+++ src/albums_migration.sql
@@ -10,10 +10,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) NOT NULL,
-    name  VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT NOT NULL,
-    genre VARCHAR(50) NOT NULL,
+    artist VARCHAR(50),
+    name  VARCHAR(50),
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
