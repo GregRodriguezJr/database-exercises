04ed2ae7b2802e273d731de2c590ea89072ea68b
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-25 15:07:38 -0500

    updated album seeder and migration

--- src/albums_migration.sql
+++ src/albums_migration.sql
@@ -10,10 +10,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name  VARCHAR(50),
-    release_date DATE,
+    artist VARCHAR(255),
+    name  VARCHAR(255),
+    release_date YEAR,
     sales FLOAT,
-    genre VARCHAR(50),
+    genre VARCHAR(255),
     PRIMARY KEY (id)
 );
